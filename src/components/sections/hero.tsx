e8358b0743fd1dec2d855a0765f27f8fafc1c929
Mariana Mazzariello <marianamazzariello2@gmail.com>
2026-05-20 12:21:20 +0200

    UPDATE y x &

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -96,7 +96,7 @@ export function Hero() {
                 color: "#ffffff",
               }}
             >
-              Y RESPONDEN
+              & RESPONDEN
             </motion.h1>
           </div>
 
